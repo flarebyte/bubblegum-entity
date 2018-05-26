a33ceae9c6eb62c74f7c20ceaa1ecef881da1569
olih <olih@users.noreply.github.com>
2018-05-26 10:19:51 +0100

    wip

--- tests/AttributeTests.elm
+++ tests/AttributeTests.elm
@@ -15,13 +15,13 @@ suite =
     describe "The Attribute module"
         [ describe "setId"
             [
-               fuzz2 fuzzyV1SetId fuzzyV2SetId "setId should work with valid parameters" <|
+               fuzz2 fuzzyV1SetId fuzzyV2SetId "setId should return a valid Model for a valid id" <|
                 \v1 v2 ->
                     Attribute.setId (validP1SetId v1) (validP2SetId v2)
                     |> summarizeSetId
                     |> Expect.equal expectedValidSetId
                 
-                , fuzz2 fuzzyV1SetId fuzzyV2SetId "setId should work when id is nothing" <|
+                , fuzz2 fuzzyV1SetId fuzzyV2SetId "setId should return a valid Model when id is nothing" <|
                 \v1 v2 ->
                     Attribute.setId Nothing (validP2SetId v2)
                     |> summarizeSetIdWithIdNothing
