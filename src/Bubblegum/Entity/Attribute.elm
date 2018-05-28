a4f4fd59a94aa221247c4a9b29d951356a1de255
olih <olih@users.noreply.github.com>
2018-05-28 12:56:33 +0100

    wip

--- src/Bubblegum/Entity/Attribute.elm
+++ src/Bubblegum/Entity/Attribute.elm
@@ -15,7 +15,15 @@ module Bubblegum.Entity.Attribute
 
 {-| Setting key
 
-@docs Model
+
+# Model setters
+
+@docs Model, setId, setKey, setValues, setFacets
+
+
+# Attributes
+
+@docs findAttributeByKey, findAttributeFirstValueByKey, replaceAttributeByKey
 
 -}
 
@@ -24,31 +32,57 @@ import Maybe
 import Tuple exposing (first, second)
 
 
-{-| The core representation of a field.
+{-| The core representation of an attribute with:
+
+  - id: a possible id to represent the attribute (ex: id:1234)
+  - key: the key of the attribute (ex: ui:label)
+  - values: a list of string values
+  - facets: an optional list of tags to mark the data (ex: [min])
+
 -}
 type alias Model =
     { id : Maybe String
     , key : String
-    , facets : List String
     , values : List String
+    , facets : List String
     }
 
 
+{-| Set a possible id to represent the attribute
+
+     setId (Just "id:1234") model
+
+-}
 setId : Maybe String -> Model -> Model
 setId id model =
     { model | id = id }
 
 
+{-| Set the key of the attribute
+
+    setKey "ui:label" model
+
+-}
 setKey : String -> Model -> Model
 setKey key model =
     { model | key = key }
 
 
+{-| Set an optional list of tags to mark the data
+
+    setFacets ["min", "inclusive"] model
+
+-}
 setFacets : List String -> Model -> Model
 setFacets facets model =
     { model | facets = facets }
 
 
+{-| Set a list of string values
+
+    setValues ["some label"] model
+
+-}
 setValues : List String -> Model -> Model
 setValues values model =
     { model | values = values }
@@ -63,6 +97,11 @@ blankAttribute =
     }
 
 
+{-| Find an attribute by key
+
+    findAttributeByKey "ui:label" models -- Just label
+
+-}
 findAttributeByKey : String -> List Model -> Maybe Model
 findAttributeByKey key attributes =
     case attributes of
@@ -75,15 +114,32 @@ findAttributeByKey key attributes =
             else
                 findAttributeByKey key rest
 
+
+{-| Delete an attribute by key
+
+    deleteAttributeByKey "ui:label" models -- []
+
+-}
 deleteAttributeByKey : String -> List Model -> List Model
 deleteAttributeByKey key attributes =
     List.filter (\attr -> attr.key /= key) attributes
 
+
+{-| Find an attribute by key and then get the first value if any
+
+    findAttributeFirstValueByKey "ui:label" models -- Just "some label"
+
+-}
 findAttributeFirstValueByKey : String -> List Model -> Maybe String
 findAttributeFirstValueByKey key attributes =
     findAttributeByKey key attributes |> Maybe.map .values |> Maybe.andThen List.head
 
 
+{-| Find an outcome searching by key
+
+    findOutcomeByKey "ui:label" models -- Valid ["some label"]
+
+-}
 findOutcomeByKey : String -> List Model -> Outcome (List String)
 findOutcomeByKey key attributes =
     findAttributeByKey key attributes |> Maybe.map .values |> Outcome.fromMaybe
@@ -94,6 +150,11 @@ createTuple a b =
     ( a, b )
 
 
+{-| Find an outcome searching by a couple of keys
+
+    findOutcomeByKeyTuple ("ui:min", "ui:max") models -- Valid (["1"], ["10"])
+
+-}
 findOutcomeByKeyTuple : ( String, String ) -> List Model -> Outcome ( List String, List String )
 findOutcomeByKeyTuple tuple attributes =
     let
@@ -109,6 +170,11 @@ findOutcomeByKeyTuple tuple attributes =
     Outcome.fromMaybe ab
 
 
+{-| Replace or create an attribute by key
+
+     replaceAttributeByKey "ui:label" ["new label"] models -- models
+
+-}
 replaceAttributeByKey : String -> List String -> List Model -> List Model
 replaceAttributeByKey key values attributes =
     let
