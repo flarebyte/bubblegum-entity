8862129cc0bba9c2012e63e75bea1f9f49ce55c8
olih <olih@users.noreply.github.com>
2018-05-28 21:29:20 +0100

    wip

--- src/Bubblegum/Entity/StateEntity.elm
+++ src/Bubblegum/Entity/StateEntity.elm
@@ -1,26 +1,31 @@
 module Bubblegum.Entity.StateEntity exposing (Model, asAttributesIn, setAttributes)
 
-{-| Setting key
+{-| A state entity represents the state that could be applied to a widget
 
-@docs Model
+@docs Model, setAttributes, asAttributesIn
 
 -}
+
 
 import Bubblegum.Entity.Attribute as Attribute
 
 
-{-| The core representation of a field.
+{-| The core representation of state entity is mostly a list of attributes.
 -}
 type alias Model =
     { attributes : List Attribute.Model
     }
 
 
+{-| Assign attributes to an entity
+-}
 setAttributes : List Attribute.Model -> Model -> Model
 setAttributes attributes model =
     { model | attributes = attributes }
 
 
+{-| Assign attributes to an entity
+-}
 asAttributesIn : Model -> List Attribute.Model -> Model
 asAttributesIn =
     flip setAttributes
--- src/Bubblegum/Entity/Tansformation.elm
+++ src/Bubblegum/Entity/Tansformation.elm
@@ -1,21 +1,25 @@
 module Bubblegum.Entity.Transformation
     exposing
         ( addStringPrefix
+        , addStringSuffix
         )
 
-{-| Setting key
+{-| List of functions that can applied to an Outcome  
 
-@docs Model
+@docs addStringPrefix, addStringSuffix
 
 -}
 
 import Bubblegum.Entity.Outcome as Outcome exposing (Outcome(..))
-import List
-import Regex
-import Set
-import Tuple exposing (first, second)
 
-
+{-| Apply a string prefix to an outcome
+-}
 addStringPrefix : String -> Outcome String -> Outcome String
 addStringPrefix prefix outcome =
     Outcome.map (\v -> prefix ++ v) outcome
+
+{-| Apply a string suffix to an outcome
+-}
+addStringSuffix : String -> Outcome String -> Outcome String
+addStringSuffix suffix outcome =
+    Outcome.map (\v -> v ++ suffix) outcome
