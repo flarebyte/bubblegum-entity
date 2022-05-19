8b8e6276dd11537ae76db6d033637ce7839b78e7
Olivier Huin <olih@users.noreply.github.com>
2022-05-19 09:28:52 +0100

    Add data for generator

---
+++ generator/src/data/data-model.ts
@@ -0,0 +1,26 @@
+import { FunctionInfo } from "../parse-elm-function";
+
+export interface MetaState {
+  name: string;
+}
+export interface FunctionMeta {
+  name: string;
+  states: MetaState[];
+}
+
+export interface ExtendedFunctionInfo extends FunctionInfo {
+  states: MetaState[];
+}
+
+export const toFunctionMetaObject = (
+  metaArray: FunctionMeta[]
+): { [name: string]: FunctionMeta } =>
+  Object.fromEntries(metaArray.map((m) => [m.name, m]));
+
+export const mergeWithExtendedMeta = (
+  functionInfo: FunctionInfo,
+  meta: FunctionMeta
+): ExtendedFunctionInfo => ({
+  ...functionInfo,
+  states: meta.states,
+});
--- generator/src/elm-io.ts
+++ generator/src/elm-io.ts
@@ -1,7 +1,14 @@
 import path from "node:path";
 import jetpack from "fs-jetpack";
 import { FunctionInfo, parseElmFunctions } from "./parse-elm-function.js";
+import {
+  FunctionMeta,
+  mergeWithExtendedMeta,
+  ExtendedFunctionInfo,
+  toFunctionMetaObject,
+} from "./data/data-model.js";
 const elmSourceDir = "../src/Bubblegum/Entity";
+const generatedDir = "../generated";
 
 export const readElmContent = async (name: string): Promise<string> => {
   const filename = path.join(elmSourceDir, `${name}.elm`);
@@ -13,9 +20,31 @@ export const readElmContent = async (name: string): Promise<string> => {
 /**
  * Load a list of elm functions
  * @param name the elm file name
- * @returns 
  */
-export const readElmFunctions = async (name: string): Promise<FunctionInfo[]> => {
+export const readElmFunctions = async (
+  name: string
+): Promise<FunctionInfo[]> => {
   const content = await readElmContent(name);
   return parseElmFunctions(content);
+};
+
+/**
+ * Delete previously generated folder
+ */
+export const deleteGenerated = async () => {
+  console.log("Deleting generated folder");
+  await jetpack.removeAsync(generatedDir);
+};
+
+export const mergeElmFunctions = async (
+  name: string,
+  metas: FunctionMeta[]
+): Promise<ExtendedFunctionInfo[]> => {
+  const elmFunctions = await readElmFunctions(name);
+  const metaDict = toFunctionMetaObject(metas);
+  const defaultFunctionMeta: FunctionMeta = { name: "", states: [] };
+  const extendedFunctions = elmFunctions.map((fn) =>
+    mergeWithExtendedMeta(fn, metaDict[fn.name] || defaultFunctionMeta)
+  );
+  return extendedFunctions;
 };
